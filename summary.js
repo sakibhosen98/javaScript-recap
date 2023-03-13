f16f4a4214cb933a3ce3d7776a463d12856c9283
sakibhosen98 <121639496+sakibhosen98@users.noreply.github.com>
2023-03-13 20:25:10 +0600

    summary

---
+++ summary.js
@@ -0,0 +1,62 @@
+/*
+Fundamentals
+  1.1 Variable(let, const) three types variable
+  1.2 condition ( <, >, ===, !==, <=, >=), &&, ||, ef, if-else, else
+  1.3  array: declare, length, index, push, pop, indexOf
+  1.4 for loop, while loop
+  1.5 function return parameter
+  1.6 Object property, including array, object
+  ------------------------------------
+
+  ES6
+  1. Template string ${}
+  2. spread (... )
+  2.1 copy an array then add a new element to an array
+  2.2 use filter to remove an element from an array
+  3. arrow function
+  3.1 no parameter ()
+  3.2 single parameter 
+  3.3 multiple parameters()
+  3.4 multi line
+  3.5 object declaration short hand
+  4. destructuring : object destructuring and array destructuring
+  5. Object shorthand
+  6. function parameter default value
+  7. Optional chaninig (?.)
+  -----------------------------------------------
+
+  Browser API
+  1. local storage session storage
+  2. local API
+  3. History API
+  4. fetch
+  --------------------------------------------------
+
+  Others :
+  1. array : map, forEach, filter, find
+  2. ternary operator
+  3. logical and logical or && ||
+  4. JSON (stringfy, parse)
+  5. +, 
+*/
+
+
+// Short hand system
+const x = 50;
+const y = 100;
+const obj = {
+  x: x,
+  y: y
+};
+// shorthand
+const obj1 = {x, y};
+ 
+const name = 'Solimullah';
+const area = 'Dhaka';
+
+const nobab = {
+  name: name,
+  area: area
+}
+// shortHand
+const nobab1 = {name, area};
